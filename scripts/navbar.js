dd0b39f605f59ce52aacc7026e13f21ef44371bc
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2023-08-17 13:35:12 -0400

    add navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,26 @@
+class Navigation extends HTMLElement {
+    constructor () {
+        super();
+    }
+    
+    connectedCallback() {
+        this.innerHTML = `
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/Website-Project/htmlFiles/index.html">Dragon Boating</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Teams</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        `
+    }
+}
+customElements.define('nav-bar', Navigation);
